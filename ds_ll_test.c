d52d39bbd73f1432d5ac7d83631d4aebf1ac359a
lelanthran <lelanthran@gmail.com>
2019-04-22 19:56:35 +0200

    Fixed compiler warnings in the test program for linked-lists.

--- ds_ll_test.c
+++ ds_ll_test.c
@@ -20,7 +20,7 @@ int main (void)
       "nine",
       "ten",
    };
-   const nelements = sizeof elements / sizeof elements[0];
+   const size_t nelements = sizeof elements / sizeof elements[0];
 
    ds_ll_t *l1 = ds_ll_ins_after (NULL, "First List");
    ds_ll_t *l2 = ds_ll_ins_after (NULL, "Second List");
@@ -30,8 +30,8 @@ int main (void)
    }
 
    for (size_t i=0; i<nelements; i++) {
-      ds_ll_t *l1_node = ds_ll_ins_tail (l1, elements[i]),
-              *l2_node = ds_ll_ins_head (l2, elements[i]);
+      ds_ll_t *l1_node = ds_ll_ins_tail (l1, (void *)elements[i]),
+              *l2_node = ds_ll_ins_head (l2, (void *)elements[i]);
 
       if (!l1_node || !l2_node) {
          fprintf (stderr, "Failed to add node to linked list\n");
@@ -45,12 +45,12 @@ int main (void)
 
    ds_ll_t *tmp = ds_ll_first (l1);
    while (tmp) {
-      printf ("[%s]\n", ds_ll_value (tmp));
+      printf ("[%s]\n", (const char *)ds_ll_value (tmp));
       tmp = ds_ll_next (tmp);
    }
    tmp = ds_ll_first (l2);
    while (tmp) {
-      printf ("[%s]\n", ds_ll_value (tmp));
+      printf ("[%s]\n", (const char *)ds_ll_value (tmp));
       tmp = ds_ll_next (tmp);
    }
 errorexit:
