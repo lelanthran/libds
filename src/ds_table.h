54b05c06b0e09408d4adecbc90e74705a3ab603c
lelanthran <lelanthran@gmail.com>
2021-04-03 02:34:16 +0200

    WIP: Double-free of rows, ds_table_row_del() not implemented.

--- src/ds_table.h
+++ src/ds_table.h
@@ -25,31 +25,31 @@ extern "C" {
    void ds_table_del (ds_table_t *table);
 
    // Get the number of rows and cols in this table
-   size_t ds_table_nrows (ds_table_t *table);
-   size_t ds_table_ncols (ds_table_t *table);
+   size_t ds_table_nrows (const ds_table_t *table);
+   size_t ds_table_ncols (const ds_table_t *table);
 
    // Create a new empty row, either at the beginning, the end or at the specified index. When
    // a row is created at a specified index all subsequent rows are shifted down. When index
    // is ot of range the new row is created at the end of the table.
-   bool ds_table_row_new_first (ds_table_t *table);
    bool ds_table_row_new (ds_table_t *table, size_t index);
+   bool ds_table_row_new_first (ds_table_t *table);
    bool ds_table_row_new_last (ds_table_t *table);
 
    // Delete a row. When a row is deleted, all subsequent rows are shifted up. Deleting a row
    // does not delete the data objects stored in the row - the caller is responsible for
    // deleting objects that are stored in the table.
-   void ds_table_row_del_first (ds_table_t *table);
    void ds_table_row_del (ds_table_t *table, size_t row);
+   void ds_table_row_del_first (ds_table_t *table);
    void ds_table_row_del_last (ds_table_t *table);
 
    // Set the value at a specific [row,col]. If the specified row and column are out of range
    // no action is performed and the function simply returns.
-   void ds_table_set (ds_table_t *table, size_t row, size_t col, void *el);
+   void ds_table_set (const ds_table_t *table, size_t row, size_t col, void *el);
 
    // Get the pointer to the object stored in the table at [row,col]. If the specified row
    // and column are out of range NULL is returned. Note that NULL will also be returned if
    // the data element at the specified [row,col] is NULL.
-   void *ds_table_get (ds_table_t *table, size_t row, size_t col);
+   void *ds_table_get (const ds_table_t *table, size_t row, size_t col);
 
    // Iterate  across the table, calling fptr() on each element. The iteration is started
    // at the first row and column, and each row is iterated before proceeding to the next
