67bedb6e31c8e30c7d2938f45bfdeffff3aeb812
lelanthran <lelanthran@gmail.com>
2024-01-04 10:35:23 +0200

    ds_set module added to build process with empty files.

---
+++ src/ds_set.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+#include "ds_set.h"
+
+
+ds_set_t *ds_set_new (void)
+{
+   return NULL;
+}
+
+void ds_set_del (ds_set_t *set)
+{
+   if (!set)
+      return;
+}
+
---
+++ src/ds_set.h
@@ -0,0 +1,22 @@
+
+#ifndef H_DS_SET
+#define H_DS_SET
+
+typedef struct ds_set_t ds_set_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+   ds_set_t *ds_set_new (void);
+   void ds_set_del (ds_set_t *set);
+
+
+#ifdef __cplusplus
+};
+#endif
+
+
+#endif
+
+
